84f9aa6180d9bdb10a7d47bb22976e839869f7ac
hxzmm <hxzmm@qq.com>
2018-10-01 14:04:13 +0800

    Update kcptun.sh

--- install/kcptun.sh
+++ install/kcptun.sh
@@ -1,7 +1,7 @@
 mkdir kcptun
 cd kcptun
 wget https://github.com/xtaci/kcptun/releases/download/v20180926/kcptun-linux-amd64-20180926.tar.gz
-tar xfvz kcptun-linux-amd64-20180810.tar.gz
+tar xfvz kcptun-linux-amd64-20180926.tar.gz
 wget https://raw.githubusercontent.com/hxzmm/kcptun/master/install/kcptun.conf
 wget -O /etc/init.d/kcptun https://raw.githubusercontent.com/hxzmm/kcptun/master/install/kcptun
 chmod 755 /etc/init.d/kcptun; update-rc.d kcptun defaults; service kcptun start
